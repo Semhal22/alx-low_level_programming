0339e8cf5bd0728133a355b7b39c219b12715f5e
Semhal Desta <semhalgidena@gmail.com>
2023-02-17 19:07:27 -0800

    Changed code

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,13 +7,11 @@
  */
 int main(void)
 {
-	char c = 'a';
+	char b;
 
-	while(c < 'z')
-	{
-		if (c != 'q' && c != 'e')
-			putchar(c);
-	}
+	for (b = 'a'; b <= 'z'; b++)
+		if (b != 'q' && b != 'e')
+			putchar(b);
 	putchar('\n');
 	return (0);
 }
