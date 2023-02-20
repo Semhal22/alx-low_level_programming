aadd83289da60e3ac2ff005598b467d487f0d83c
Semhal Desta <semhalgidena@gmail.com>
2023-02-20 13:28:40 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Print alphabets except q and e
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
