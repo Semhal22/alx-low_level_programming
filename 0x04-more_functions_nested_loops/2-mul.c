a2cee098ccb14f4a0a908752606b96aac61b7596
Semhal Desta <semhalgidena@gmail.com>
2023-02-23 11:26:02 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * mul - A function that multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 0. Checks for uppercase
 1. Checks for a digit 0 through 9
+2. Function that multiplies two integers
