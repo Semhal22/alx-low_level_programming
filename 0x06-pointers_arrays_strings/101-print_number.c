6c263e4bc3788e2d40f1254cf7af8204b0133f67
Semhal Desta <semhalgidena@gmail.com>
2023-03-02 12:07:31 -0800

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_number - prints an integer using _putchar
+ * @n: integer to be printed
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -1 * n;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -8,3 +8,4 @@ from the src
 6. A function that capitalizes all words of a string
 7. Encodes a string into 1337
 8. A function that encodes a string using rot13
+9. A function that prints an integer
