53c9c1d961c81d8d8b8ad31110501bef1c2c73ca
Semhal Desta <semhalgidena@gmail.com>
2023-03-02 12:13:36 -0800

    Added an unsigned variable

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_number - prints an integer using _putchar
  * @n: integer to be printed
@@ -6,13 +7,17 @@
  */
 void print_number(int n)
 {
+	unsigned int num;
+
+	num = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -1 * n;
+		num = -n;
 	}
 
-	if (n / 10 != 0)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+	if (num / 10 != 0)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
 }
