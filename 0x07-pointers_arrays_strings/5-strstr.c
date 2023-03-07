a5018fd872048746ee8c8fc78bf3eb66f9402b69
Semhal Desta <semhalgidena@gmail.com>
2023-03-06 17:32:09 -0800

    Modified name of variable

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -21,7 +21,7 @@ char *_strstr(char *haystack, char *needle)
 				if (needle[i + 1] == '\0')
 					return (haystack);
 				i++;
-			} while (haystack[index] == needle[index]);
+			} while (haystack[i] == needle[i]);
 		}
 
 		haystack++;
