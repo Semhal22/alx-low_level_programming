f347fccc40cc891125c5402c925cb63af7c5b070
Semhal Desta <semhalgidena@gmail.com>
2023-03-06 16:23:47 -0800

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+/**
+ * _memcpy - copies memory area
+ * @dest: destination
+ * @src: source
+ * @n: how many bytes it copies
+ * Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,1 +1,2 @@
 A function that fills memory with a constant byte
+1. A function that copies memory area
