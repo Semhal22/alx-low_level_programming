9fca0564ed63dc8cd4f0c77e3deadeafefdf22ea
Semhal Desta <semhalgidena@gmail.com>
2023-03-08 07:56:24 -0800

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+/**
+ * _pow_recursion - calulates power of a number
+ * @x: number to be raised by y
+ * @y: number to raise x
+ *
+ * Return: power of number
+ * -1 if y is lower than 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
