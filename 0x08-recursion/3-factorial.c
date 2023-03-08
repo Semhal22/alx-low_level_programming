721fb084abc1ed6773291a6115fc0b3947b383f0
Semhal Desta <semhalgidena@gmail.com>
2023-03-08 07:43:29 -0800

    Corrected = with == in the if statement

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,13 +2,13 @@
  * factorial - calculates factorial of a number
  * @n: number to be calculated
  * Return: factorial of the number
+ * -1 if number is less than 0
  */
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if ((n = 0) || (n = 1))
+	else if (n == 1 || n == 0)
 		return (1);
-	else
-		return (n * factorial(n - 1));
+	return (n * factorial(n - 1));
 }
