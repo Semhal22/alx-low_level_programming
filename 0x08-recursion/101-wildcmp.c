097343bcfcd37b1eff04b46479317fd735f6d494
Semhal Desta <semhalgidena@gmail.com>
2023-03-08 23:24:44 -0800

    Added the exact c file

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,36 @@
+/**
+ * _strcmp - compares two sdrome("step on no pets");
+					    printf("%d\n", r);
+					        return (0);
+trings
+ * @str1: first string
+ * @str2: second string
+ * @i: index of first string
+ * @j: index of second string
+ *
+ * Return: 1 if similar, 0 if not
+ */
+int _strcmp(char *str1, char *str2, int i, int j)
+{
+	if (str1[i] == '\0' && str2[j] == '\0')
+		return (1);
+	if (str1[i] == str2[j])
+		return (_strcmp(str1, str2, i + 1, j + 1));
+	if (str1[i] == '\0' && str2[j] == '*')
+		return (_strcmp(str1, str2, i, j + 1));
+	if (str2[j] == '*')
+		return (_strcmp(str1, str2, i + 1, j) || _strcmp(str1, str2, i, j + 1));
+	return (0);
+}
+/**
+ * wildcmp - calls another function _strcmp
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 1 if the strings can be considered identical
+ * 0 if not
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (_strcmp(s1, s2, 0, 0));
+}
