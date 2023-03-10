5e44d4beb28e2f46176190c8ace600eeac29c2ec
Semhal Desta <semhalgidena@gmail.com>
2023-03-10 12:22:35 -0800

    included header file

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - prints arguments it receives
  * @argc: number of arguments
@@ -9,10 +10,9 @@ int main(int argc, char *argv[])
 {
 	int i;
 	
-	if (argc > 0)
-		for (i = 0; i < argc; i++)
-		{
-			printf("%s\n", argv[i]);
-		}
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 	return (0);
 }
