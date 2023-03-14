eb14d596f6e065b8c1e2d28cd8801e058473e48f
Semhal Desta <semhalgidena@gmail.com>
2023-03-14 02:05:46 -0700

    Returns Null if something fails

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,7 +20,10 @@ int **alloc_grid(int width, int height)
 		while (i < height)
 		{
 			arr[i] = malloc(width * sizeof(int));
-			i++;
+			if (arr[i])
+				i++;
+			else
+				return (NULL);
 		}
 		for (i = 0; i < height; i++)
 			for (j = 0; j < width; j++)
