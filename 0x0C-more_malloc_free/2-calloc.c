4a5c4da21008c893d11205b5b8b003675b0bbcdd
Semhal Desta <semhalgidena@gmail.com>
2023-03-16 13:54:53 -0700

    Set each allocated byte to 0 using memset

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,22 @@
 #include <stdlib.h>
 /**
+ * _memset - fills the first n bytes of memory by the constant byte
+ * @s: starting address
+ * @b: constant byte to replace
+ * @n: n bytes of memory
+ *
+ * Return: a pointer to the memory
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	char *start = s;
+
+	for (i = 0; i < n; i++)
+		*s++ = b;
+	return (start);
+}
+/**
  * _calloc - allocates memory for an array of nmemb elements
  * of size bytes each
  * @nmemb: number of elements of array
@@ -10,15 +27,13 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *array;
-	unsigned int i;
+	void *array;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	array = malloc(nmemb * size);
 	if (array == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
-		array[0] = 0;
+	_memset(array, 0, nmemb * size);
 	return (array);
 }
