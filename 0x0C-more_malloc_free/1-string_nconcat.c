c2268bb2f7549a8190659141915b7a5f33928ce3
Semhal Desta <semhalgidena@gmail.com>
2023-03-16 13:09:48 -0700

    Allocated correct space for concatenated string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,29 +7,11 @@
  */
 int _strlen(char *str)
 {
-	unsigned int len = 0;
+	int len = 0;
 
 	while (str[len] != '\0')
 		len++;
 	return (len);
-}
-/**
- * _strncpy - copies a string
- * @dest: destination string
- * @src: source string
- * @n: number of bytes to copy from src
- *
- * Return: dest string
- */
-char *_strncpy(char *dest, char *src, int n)
-{
-	int i;
-
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for ( ; i < n; i++)
-		dest[i] = '\0';
-	return (dest);
 }
 /**
  * string_nconcat - concatenates two strings
@@ -42,24 +24,26 @@ char *_strncpy(char *dest, char *src, int n)
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, len1;
+	int i, j, num;
 	char *new_str;
 
-	len1 = _strlen(s1);
-	if (*s1 == '\0')
-		_strncpy(s1, "", sizeof(s1));
-	new_str = malloc(sizeof(char) * (len1 + n + 1));
+	num = n;
+	
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (num >= _strlen(s2))
+		num = _strlen(s2);
+	new_str = malloc(sizeof(char) * (_strlen(s1) + num + 1));
 	if (!new_str)
 		return (NULL);
-	for (i = 0; i < len1; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		new_str[i] = s1[i];
-	for (j = 0; j < n; j++)
+	for (j = 0; j < num; j++)
 	{
-		if (s2[n])
-		{
-			new_str[i] = s2[j];
-			i++;
-		}
+		new_str[i] = s2[j];
+		i++;
 	}
 	new_str[i] = '\0';
 	return (new_str);
