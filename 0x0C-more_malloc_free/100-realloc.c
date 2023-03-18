7a4330aac87545eb2c17db4f2f27e83060a63bad
Semhal Desta <semhalgidena@gmail.com>
2023-03-18 10:54:55 -0700

    Copied allocated values from ptr to reptr

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,13 +11,8 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *reptr;
+	int i;
 
-	if (new_size > old_size)
-	{
-		reptr = malloc(new_size);
-		free(ptr);
-		return (reptr);
-	}
 	if (new_size == old_size)
 		return (ptr);
 	if (ptr == NULL)
@@ -32,5 +27,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 	reptr = malloc(new_size);
+	for (i = 0; i < new_size; i++)
+		if (i < old_size)
+			reptr[i] = (char *)ptr[i];
+	free(ptr);
 	return (reptr);
 }
