2666ce38a7c1349bc6e87e77cbca9708f2add9d8
Semhal Desta <semhalgidena@gmail.com>
2023-03-20 13:42:29 -0700

    Header file that contains definition of struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+
+/**
+ * struct dog - creates a structure dog
+ * @name: name of the dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ *
+ * Description: Creates a new struct of dog which
+ * has contain name, age and owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
