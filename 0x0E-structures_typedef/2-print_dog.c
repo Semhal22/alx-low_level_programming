88af71a798b721b3779c46b0c7c702bec4374099
Semhal Desta <semhalgidena@gmail.com>
2023-03-20 15:23:39 -0700

    Exits code when passed pointer is NULL

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 /**
  * print_dog -prints a struct dog
@@ -6,16 +7,18 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d->name)
-		printf("Name: %s\n", d->name);
-	else
+	if (d == NULL)
+		exit(1);
+	if (d->name == NULL)
 		printf("Name: (nil)\n");
-	if (d->age)
-		printf("Age: %f\n", d->age);
 	else
+		printf("Name: %s\n", d->name);
+	if (d->age < 0)
 		printf("Age: (nil)\n");
-	if (d->owner)
-		printf("Owner: %s\n", d->owner);
 	else
+		printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
