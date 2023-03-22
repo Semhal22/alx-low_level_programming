add8bd14fc0a6c2dfe180e8d228c246db9aed8d8
Semhal Desta <semhalgidena@gmail.com>
2023-03-22 11:09:41 -0700

    Added README and header file

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+0x0F. C - Function pointers
+	This project contains tasks which are based on the concept of pointers to functions.
+	0. What's my name: A function that prints a name
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef FUNC_PTR
+#define FUNC_PTR
+/* Headers for the 0x0F-fuction_pointers project */
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+#endif
