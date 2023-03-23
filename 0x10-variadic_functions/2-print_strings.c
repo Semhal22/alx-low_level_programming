f980185f675c932a7afbe2aad7552c53e38bf254
Semhal Desta <semhalgidena@gmail.com>
2023-03-23 11:19:13 -0700

    Prints separator if its not NULL

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(args, char *);
-		if (i != 0)
+		if (i != 0 && separator != NULL)
 			printf("%s", separator);
 		if (str == NULL)
 			printf("%s", "(nil)");
