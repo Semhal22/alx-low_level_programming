2e0e517f8326beee559ece6286c54678bf7c64f3
Semhal Desta <semhalgidena@gmail.com>
2023-04-03 12:48:19 -0700

    Changed _putchar to printf

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,15 +1,6 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-/**
- * print_int - Prints an integer recursively
- * @num: THe integer
- */
-void print_int(int num)
-{
-	if (num > 9)
-		print_int(num / 10);
-	_putchar(num % 10 + '0');
-}
 /**
  * print_listint - Prints elements of a list
  * @h: pointer to the list
@@ -23,8 +14,7 @@ size_t print_listint(const listint_t *h)
 
 	while (temp != NULL)
 	{
-		print_int(temp->n);
-		_putchar('\n');
+		printf("%d\n", temp->n);
 		count++;
 		temp = temp->next;
 	}
