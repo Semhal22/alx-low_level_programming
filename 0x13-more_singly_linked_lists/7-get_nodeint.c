9975216ceb88837d8b895575b3a43be4d88b583b
Semhal Desta <semhalgidena@gmail.com>
2023-04-03 23:49:36 -0700

    Check if list is NULL

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,6 +13,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp = head;
 	size_t i;
 
+	if (temp == NULL)
+		return (NULL);
 	for (i = 0; i < index; i++)
 		temp = temp->next;
 	if (temp == NULL)
