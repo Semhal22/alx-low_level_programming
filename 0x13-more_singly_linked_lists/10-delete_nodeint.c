221680e1caed057adf28e1861f6fd22c119a4719
Semhal Desta <semhalgidena@gmail.com>
2023-04-04 13:44:05 -0700

    If head is NULL, it just returns

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *current = *head;
 
 	if (*head == NULL)
-		return (-1);
+		return;
 	if (index == 0)
 	{
 		*head = current->next;
