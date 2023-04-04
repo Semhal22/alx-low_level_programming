afce84928db1c95d5f0c461a4805b91f926f8eaf
Semhal Desta <semhalgidena@gmail.com>
2023-04-04 05:52:20 -0700

    Accounted for index is 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 /**
  * insert_nodeint_at_index - Inserts a new node
@@ -20,14 +21,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	insertedNode->n = n;
 	insertedNode->next = NULL;
+	if (idx == 0 && temp != NULL)
+	{
+		insertedNode->next = *head;
+		*head = insertedNode;
+		return (insertedNode);
+	}
 	while (temp != NULL)
 	{
-		if (idx == 0 && count == 0)
-		{
-			insertedNode->next = temp;
-			temp = insertedNode;
-			return (insertedNode);
-		}
 		if (count == (idx - 1))
 		{
 			insertedNode->next = temp->next;
