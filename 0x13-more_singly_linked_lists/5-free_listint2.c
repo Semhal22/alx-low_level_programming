da8000b16ffa4c8ca498d8acdfbf3338da1aaf7e
Semhal Desta <semhalgidena@gmail.com>
2023-04-03 23:00:05 -0700

    Freed both pointer to head and head

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,7 +9,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 		return;
 	while (*head != NULL)
 	{
@@ -18,4 +18,5 @@ void free_listint2(listint_t **head)
 		free(temp);
 	}
 	*head = NULL;
+	head = NULL;
 }
