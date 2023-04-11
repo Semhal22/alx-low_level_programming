fedc47b1f29513b82274122fac86e54276770c55
Semhal Desta <semhalgidena@gmail.com>
2023-04-10 19:56:40 -0700

    Copied conten of file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include "main.h"
+/**
+ * main - Content of a file to another file
+ * @argc: Argument count
+ * @argv: Pointer to arrays of arguments
+ *
+ * Return: 0 if success
+ */
+int main(int argc, char **argv)
+{
+	char *file_from, *file_to;
+	int fd_from, fd_to, status_from, status_to;
+	char buffer[1024];
+	ssize_t bytes_read, bytes_written;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "USAGE: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+	file_from = argv[1];
+	file_to = argv[2];
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", file_from);
+		exit(98);
+	}
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+	while ((bytes_read = read(fd_from, &buffer, 1024)) > 0)
+	{
+		bytes_written = write(fd_to, &buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			exit(99);
+		}
+	}
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	status_from = close(fd_from);
+	status_to = close(fd_to);
+	if (status_from == -1 || status_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (status_from == -1) ? fd_from : fd_to);
+		exit(100);
+	}
+	return (0);
+}
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -3,4 +3,5 @@ This project includes tasks about system calls.
 	0. Tread lightly, she is near - Reads a text and prints it to std output 
 	1. Under the snow - A function that creates a file 
 	2. Speak gently, she can hear - Appends text at the end of a file 
+	3. cp - A program that copies the content of a file to another file 
 	
