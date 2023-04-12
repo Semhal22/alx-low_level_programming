8f241003e0f433df8ec7e13f38612baebc371153
Semhal Desta <semhalgidena@gmail.com>
2023-04-12 02:04:40 -0700

    Split function into two functions

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,42 +5,34 @@
 #include <errno.h>
 #include "main.h"
 /**
- * main - Content of a file to another file
- * @argc: Argument count
- * @argv: Pointer to arrays of arguments
+ * cp - Copies content of file to another
+ * @file_from: File to copy from
+ * @file_to: File to copy to
  *
- * Return: 0 if success
+ * Return: 0 if successful
  */
-int main(int argc, char **argv)
+int cp(char *file_from, char *file_to)
 {
-	char *file_from, *file_to;
-	int fd_from, fd_to, status_from, status_to;
 	char buffer[1024];
+	char *str;
 	ssize_t bytes_read, bytes_written;
-	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	int fd_from, fd_to, status_from, status_to;
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "USAGE: %s file_from file_to\n", argv[0]);
-		exit(97);
-	}
-	file_from = argv[1];
-	file_to = argv[2];
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", file_from);
 		exit(98);
 	}
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, mode);
+	fd_to = open(file_to, O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 		exit(99);
 	}
-	while ((bytes_read = read(fd_from, &buffer, 1024)) > 0)
+	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
 	{
-		bytes_written = write(fd_to, &buffer, bytes_read);
+		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
@@ -56,8 +48,25 @@ int main(int argc, char **argv)
 	status_to = close(fd_to);
 	if (status_from == -1 || status_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (status_from == -1) ? fd_from : fd_to);
+		str = "Error: Can't close fd ";
+		dprintf(STDERR_FILENO, "%s%d\n", str, (status_from == -1) ? fd_from : fd_to);
 		exit(100);
 	}
 	return (0);
+}
+/**
+ * main - Calls function copy(cp)
+ * @argc: Argument count
+ * @argv: Pointer to arrays of arguments
+ *
+ * Return: 0 if success
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "USAGE: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+	return (cp(argv[1], argv[2]));
 }
