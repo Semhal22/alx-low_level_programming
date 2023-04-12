7fc129ac14a40f425393dedc59e6ab554f57c02e
Semhal Desta <semhalgidena@gmail.com>
2023-04-12 02:10:26 -0700

    Changed USAGE to Usage

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -65,7 +65,7 @@ int main(int argc, char **argv)
 {
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "USAGE: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	return (cp(argv[1], argv[2]));
