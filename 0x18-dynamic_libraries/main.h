946faf9deb77033c4217c079977509a78af75a88
Semhal Desta <semhalgidena@gmail.com>
2023-05-22 19:33:49 -0700

    Dynamic library to be used in Python

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,7 @@
 #ifndef DYNAMIC_LIBRARY
 #define DYNAMIC_LIBRARY
-#include <stdlib.h>
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,4 +22,9 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
 #endif /*DYNAMIC_LIBRARY*/
