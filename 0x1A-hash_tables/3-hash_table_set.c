c27326b57a9746a8b581ad8cd09ccc5de2b15fc9
Semhal Desta <semhalgidena@gmail.com>
2023-06-09 04:47:59 -0700

    Check if key or value is NUL

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -27,7 +27,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node, *head, *temp = NULL;
 	size_t index, update = 0;
 
-	if (ht == NULL || ht->size <= 0 || strlen(key) == 0)
+	if (ht == NULL || key == NULL || strlen(key) == 0 || value == NULL)
 		return (0);
 	index = key_index((const unsigned char *) key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
