d436076d8ba1a5ff9e459a3aff43c2031ca37c89
Semhal Desta <semhalgidena@gmail.com>
2023-06-09 01:37:11 -0700

    Remove pointer of arrays intialization with NULL

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,6 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = NULL;
-	size_t i;
 
 	if (size <= 0)
 		return (NULL);
@@ -20,7 +19,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = malloc(sizeof(hash_node_t) * table->size);
 	if (table->array == NULL)
 		return (NULL);
-	for (i = 0; i < table->size; i++)
-		table->array[i] = NULL;
 	return (table);
 }
