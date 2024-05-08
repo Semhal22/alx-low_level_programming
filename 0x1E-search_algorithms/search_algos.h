fb52f003a7fe7e0f8c69fdf8e10174e97649382b
Semhal22 <123024495+Semhal22@users.noreply.github.com>
2024-05-09 01:45:05 +0200

    Delete 0x1E-search_algorithms directory

--- 0x1E-search_algorithms/search_algos.h
+++
@@ -1,6 +0,0 @@
-#ifndef SEARCH_ALGORITHM
-#define SEARCH_ALGORITHM
-/* Prototypes for functions used in the project*/
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-#endif
